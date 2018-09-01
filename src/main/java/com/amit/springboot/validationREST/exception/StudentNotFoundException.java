5c5feb9329ed60d7f9eefcbf19dfd4dab20f4561
amit.naikwadi84@gmail.com <amit.naikwadi84@gmail.com>
2018-09-01 20:01:41 +0530

    Spring Boot REST validation example

---
+++ src/main/java/com/amit/springboot/validationREST/exception/StudentNotFoundException.java
@@ -0,0 +1,15 @@
+package com.amit.springboot.validationREST.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String exception) {
+
+		super(exception);
+
+	}
+
+}
